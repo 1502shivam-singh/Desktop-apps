4a625ceadf42170ea5c318bad2db01200509572b
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-09-27 23:44:53 +0530

    Added algorithm to parse input and evaluate

--- Scientific calculator/mainwindow.cpp
+++ Scientific calculator/mainwindow.cpp
@@ -1,7 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QStack>
+#include <QTextStream>
 #include <QVector>
+
+QTextStream out(stdout);
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
     connect(ui->p8, SIGNAL(released()), this, SLOT(handlePush()));
     connect(ui->p9, SIGNAL(released()), this, SLOT(handlePush()));
     connect(ui->p0, SIGNAL(released()), this, SLOT(handlePush()));
+    connect(ui->openB, SIGNAL(released()), this, SLOT(handlePush()));
+    connect(ui->closeB, SIGNAL(released()), this, SLOT(handlePush()));
     connect(ui->back, SIGNAL(released()), this, SLOT(handleBack()));
     connect(ui->subtract, SIGNAL(released()), this, SLOT(handlePush()));
     connect(ui->mult, SIGNAL(released()), this, SLOT(handlePush()));
@@ -31,6 +36,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+int prec(QChar sym)
+{
+    if(sym == 'x' || sym == '/')
+    {
+        return 2;
+    }
+    else if(sym =='+' || sym == '-')
+    {
+        return 1;
+    }
+    else return 0;
+}
 
 void MainWindow::handlePush(){
     QPushButton* pButton = qobject_cast<QPushButton*>(sender());
@@ -55,54 +72,148 @@ void MainWindow::handleBack(){
 
 void MainWindow::handleResult(){
     std::cout<<"Pressed";
+    QStack<QChar> symHold;
+    QVector<int> numbers;
+    QVector<QChar> symbols;
+    QVector<int> postExp;
+    QVector<QChar> postSym;
     QStack<int> numHold;
-    QVector<QChar> mexpr;
-    int pv = 0, num = 0;
+    int pv = 0, num = 0, j=0;
     QString expression = ui->label->text();
-    for(int i = expression.size()-1; i>=0; i--){
-        if(expression.at(i) == '-'||(expression.at(i) == '/'||(expression.at(i) == '+'||(expression.at(i) == 'x')))){
-            numHold.push(num);
+
+    for(int i = expression.size()-1; i>=0; i--)
+    {
+        if(expression.at(i) == '-'||(expression.at(i) == '/'||(expression.at(i) == '+'||(expression.at(i) == 'x'))))
+        {
+            numbers.push_back(num);
+            numbers.push_back(' ');
+            symbols.push_back(expression.at(i));
             num=0;
             pv=0;
         }
-        else{
+        else
+        {
             num += expression.at(i).digitValue()*pow(10,pv);
             pv++;
-            if(i == 0){
-                numHold.push(num);
+            if(i == 0)
+            {
+                numbers.push_back(num);
                 num=0;
                 pv=0;
             }
         }
     }
-    for(int i = 0; i >expression.size(); i++){
-        if(expression.at(i) == '-'||(expression.at(i) == '/'||(expression.at(i) == '+'||(expression.at(i) == 'x')))){
-            if(!numHold.isEmpty()){
-                mexpr.push_back(numHold.pop());
-                mexpr.push_back(expression.at(i));
+
+    j = symbols.size()-1;
+
+    for(int i=numbers.size()-1; i>=0; i--){
+        if((char)numbers[i]==' ')
+        {
+            if(symbols.at(j)!=')' && symbols.at(j)!='(')
+            {
+                while(!symHold.isEmpty() && (prec(symbols.at(j))<=prec(symHold.top())))
+                {
+                    postSym.push_back(symHold.pop());
+                    postExp.push_back(' ');
+                }
+                symHold.push(symbols.at(j));
+                j--;
+            }
+            else
+            {
+                if(symbols.at(j)==')')
+                {
+                    while(!symHold.isEmpty() && symHold.top()=='(')
+                    {
+                        postSym.push_back(symHold.pop());
+                        postExp.push_back(' ');
+                    }
+                    symHold.pop();
+                    j--;
+                }
+                else
+                {
+                    symHold.push(symbols.at(j));
+                    j--;
+                }
             }
         }
-        else{
-            if(!numHold.isEmpty()){
-                mexpr.push_back(numHold.pop());
+        else
+        {
+            postExp.push_back(numbers[i]);
+        }
+    }
+
+    while(!symHold.isEmpty())
+    {
+        postSym.push_back(symHold.pop());
+        postExp.push_back(' ');
+    }
+
+    numbers.clear();
+    symbols.clear();
+    j = 0;
+    //postExp now has the postfix expression in parsed form
+    //Along with this postSym has the symbols in the expression in parsed form
+
+    for (int i = 0; i < postExp.size(); ++i)
+    {
+        if((char)postExp[i] != ' ')
+        {
+            numHold.push(postExp[i]);
+        }
+        else
+        {
+            int val1 = numHold.pop();
+            int val2 = numHold.pop();
+            switch (postSym.at(j).unicode())
+            {
+                case '+':   numHold.push(val2+val1);break;
+                case '-':   numHold.push(val2-val1);break;
+                case 'x':   numHold.push(val2*val1);break;
+                case '/':   numHold.push(val2/val1);break;
             }
         }
     }
-    for(int i = 0; i >mexpr.size(); i++){
-        std::cout<<mexpr[i].digitValue();
-    }
+
+    ui->label->setText(QString::number(numHold.pop()));
 }
 
-//stack<int> numStore;
-//Int i = 0, num = 0;
 
-//for(in end….0){
-//    if(val == {+. -. *, /}){
-//        numStore.push(num);
-//    num=0;i=0;
-//}
-//num += val.toInt()*pow(10,i);
-//    i++;
-//}
+/*
 
+What do i have ?????????
 
+String representing the expression -- expression
+
+Array of ints (stack or something)
+
+what i expect to do ---
+    create a vector containing the ints and the symbols {+.-.x,/} basically the expression
+
+    convert this to postfix expression
+
+    for example -   4 - (1 + 2) + (2 + 3) this expression evalutes to below in postfix -
+    4,1,2.+.-.2,3,+.+   store as two arrays
+    [4,1,2,' ',' ',2,3,' ',' '] where ' ' means a symbol, which can be traced in  another operator array
+    [+.-.+,+]
+
+    evaluate the expression using a stack
+
+    push into the stack, when encounter a symbol then trace the symbol array.
+
+    Algorithm to convert individual digits in the expression string to individual numbers
+
+    stack<int> numStore;
+    Int i = 0, num = 0;
+
+    for(in end….0){
+        if(val == {+. -. *, /}){
+            numStore.push(num);
+        num=0;i=0;
+    }
+    num += val.toInt()*pow(10,i);
+    i++;
+    }
+
+*/
